2037e87912b4f23ab9f3eafc5687f53640231b31
Nihal Potdar <Nihalpot2002@gmail.com>
2020-10-11 13:44:41 -0600

    improved regex performance

--- detect.py
+++ detect.py
@@ -5,10 +5,10 @@ import argparse
 
 # Imports the Google Cloud client library
 from google.cloud import vision
-import write_to_excel
+from write_to_excel import write
 
 client = vision.ImageAnnotatorClient()
-parser = argparse.ArgumentParser(description='Transcrible to an excel file')
+parser = argparse.ArgumentParser(description='Transcribe to an excel file')
 parser.add_argument('--folder-path', type=str, help='the path of the folder with images on the local directory', required=True)
 parser.add_argument('--csv-path', type=str, help='the name or path of the csv file to be created')
 args = parser.parse_args()
@@ -17,19 +17,20 @@ data_rows = []
 
 patterns = {
     "receipt": "Receipt[ :][0-9][0-9][0-9][0-9]",
-    "date":  "Date.*Received",
-    "recieved_name": "Received With Thanks From.*Address",
-    "address": "Address.*Ph",
-    "phone_number": "Ph[ :]+[ \d-]*",
-    "amount": "\$[_\d]*"
+    "date":  r"^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$ | \w+\s\d+(st)?(nd)?(rd)?(th)?,\s+\d+ | \d{4}[-/ ]\d{2}[-/ ]\d{2}",
+    "recieved_name": r"Received With Thanks From:[ a-zA-Z]*(Address)*",
+    "address": r"Address[a-zA-Z0-9,.() ]*Ph | [0-9]+[a-zA-Z,.() ]*NW | [0-9]+[a-zA-Z,.() ]*SW | [0-9]+[a-zA-Z,.() ]*NE | [0-9]+[a-zA-Z,.() ]*SE | \s*([0-9]*)\s((NW|SW|SE|NE|S|N|E|W))?(.*)((NW|SW|SE|NE|S|N|E|W))?((#|APT|BSMT|BLDG|DEPT|FL|FRNT|HNGR|KEY|LBBY|LOT|LOWR|OFC|PH|PIER|REAR|RM|SIDE|SLIP|SPC|STOP|STE|TRLR|UNIT|UPPR|\,)[^,]*)(\,)([\s\w]*)\n",
+    "phone_number": r"\d{3}[-\.\s]??\d{3}[-\.\s]??\d{3,4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{3,4}|\d{3}[-\.\s]??\d{3,4}",
+    "amount": "\$[ _\d]*"
 }
 special_conditions = {
-    "recieved name": ["Received With Thanks From", "Address"],
+    "recieved_name": ["Received With Thanks From", "Address"],
     "address": ["Ph"],
     "phone_number": ['Ph'],
     "amount": ["$"],
     "date": ["Received"]
 }
+duplicate_number = '(403) 668-0653'
 
 data = {}
 
@@ -60,11 +61,20 @@ def detect_document(path):
     
     for col in patterns:
         entry = re.search(patterns[col], text)
-    
-        try:
-            entry = entry.group()
-        except:
+        
+        if col == "phone_number":
+            extend = re.findall(patterns[col], text)
             entry = ""
+            for i in extend:
+                if i == duplicate_number:
+                    continue
+                elif '403' in i or '587' in i:
+                    entry = i
+        else:
+            try:
+                entry = entry.group()
+            except:
+                entry = ""
 
         if col in special_conditions:
             for i in special_conditions[col]:
@@ -75,13 +85,16 @@ def detect_document(path):
         if col == "recieved name" and ('inc' in data[col] or 'ltd' in data[col]):
             data['additional comments'] = 'corp'
 
+    print(data) # test
     data_rows.append(list(data.values()))
 
 if __name__ == '__main__':
     folder_path = args.folder_path
     csv_path = args.csv_path
-    print(folder_path)
-    print(csv_path)
     
+    #detect_document(os.path.join(folder_path, 'np.jpg'))
     for image_path in os.listdir(folder_path):
-        detect_document(os.path.join(os.getcwd(), "test22.jpg"))
+        detect_document(os.path.join(folder_path, image_path))
+        data = {}
+
+    write(csv_path, data_rows)
--- write_to_excel.py
+++ write_to_excel.py
@@ -3,18 +3,19 @@ import pandas as pd
 def create_dataframe_from_list(lst):
     return pd.DataFrame(lst)
 
-def write_to_excel(file_name,*args):
-    big_list, cols = [], []
+def write(file_name,big_list,*args):
+    cols = []
 
+    '''
     for key in args:
         if isinstance(key, list):
             big_list.append(key)
 
         if isinstance(key, str):
             cols.append(key)
-
-    df = pd.DataFrame(list(zip(*big_list)))
+    '''
+    df = pd.DataFrame(big_list)
     df.to_csv(file_name, mode='a', index = False, header=None)
 
 if __name__ == "__main__":
-    write_to_excel('pandas_simple.csv', [8,9,10], [3,4,5], 'test1', 'test2')
+    write('pandas_simple.csv', [[8,9,10], [3,4,5]], 'test1', 'test2')
